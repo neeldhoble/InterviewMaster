a9bb3d31ea6c15cc775c862f4bb0d6f11c82ed48
Humesh Deshmukh <112746090+humeshdeshmukh@users.noreply.github.com>
2024-12-02 11:02:38 +0530

    new changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
 import { cn } from "@/lib/utils";
 import { Noto_Sans } from 'next/font/google';
