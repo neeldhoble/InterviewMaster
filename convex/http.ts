3d4cbe88429a10633ee6c24c83f97297e0092587
Humesh Deshmukh <112746090+humeshdeshmukh@users.noreply.github.com>
2024-12-22 20:26:50 +0530

    new changes

--- convex/http.ts
+++ convex/http.ts
@@ -1,17 +1,12 @@
-import { auth } from "./auth";
-import { httpRouter } from "convex/server";
-import { httpAction } from "./_generated/server";
+import { httpRouter } from "./_generated/server";
 import { internal } from "./_generated/api";
 
 const http = httpRouter();
 
-auth.addHttpRoutes(http);
-
-// Route for Stripe webhook
 http.route({
     path: "/stripe",
     method: "POST",
-    handler: httpAction(async (ctx, request) => {
+    handler: async (ctx, request) => {
         const signature = request.headers.get("stripe-signature") as string;
         const res = await ctx.runAction(internal.stripe.fulfill, {
             signature, 
@@ -26,7 +21,8 @@ http.route({
                 status: 400
             })
         }
-    })
-})
+    }
+});
 
 export default http;
+
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,46 +1,40 @@
+ import "./globals.css";
 
-import "./globals.css";
+ import { Analytics } from "@vercel/analytics/react"
+ import { cn } from "@/lib/utils";
+ import { Noto_Sans } from 'next/font/google';
+ import { generateMetadata } from "@/lib/generateMetadata";
+ import { ConvexClientProvider } from "@/providers/ConvexAuthProvider";
+ import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
 
-import { Analytics } from "@vercel/analytics/react"
-import { cn } from "@/lib/utils";
-import { Noto_Sans } from 'next/font/google';
-// import { Analytics } from "@/lib/analytics/Analytics";
-import { generateMetadata } from "@/lib/generateMetadata";
-import { ConvexClientProvider } from "@/providers/ConvexAuthProvider";
-import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
+ import NextTopLoader from 'nextjs-toploader';
+ import { Header } from "@/components/Header";
+ import { Footer } from "@/components/Footer";
+ import { Toaster } from "@/components/ui/sonner";
 
-import NextTopLoader from 'nextjs-toploader';
-import { Header } from "@/components/Header";
-import { Footer } from "@/components/Footer";
-import { Toaster } from "@/components/ui/sonner";
+ const font = Noto_Sans({ weight: ['400', '700'], subsets: ['latin'] });
 
+ export const metadata = generateMetadata();
 
-const font = Noto_Sans({ weight: ['400', '700'], subsets: ['latin'] });
-
-export const metadata = generateMetadata();
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <ConvexAuthNextjsServerProvider>
-      <html lang="en" suppressHydrationWarning>
-        <body
-          className={cn(font.className, "antialiased")}
-        >
-          <ConvexClientProvider>
-            {/* TODO: Use a different color here based on your app theme */}
-            <NextTopLoader showSpinner={false} color="#fcba28" />
-            <Toaster />
-            <Header />
-            {children}
-            <Footer />
-          </ConvexClientProvider>
-        </body>
-        <Analytics />
-      </html>
-    </ConvexAuthNextjsServerProvider>
-  );
-}
+ export default function RootLayout({
+   children,
+ }: Readonly<{
+   children: React.ReactNode;
+ }>) {
+   return (
+     <ConvexAuthNextjsServerProvider>
+       <html lang="en" suppressHydrationWarning>
+         <body className={cn(font.className, "antialiased")}>
+           <ConvexClientProvider>
+             <NextTopLoader showSpinner={false} color="#fcba28" />
+             <Toaster />
+             <Header />
+             {children}
+             <Footer />
+             <Analytics />
+           </ConvexClientProvider>
+         </body>
+       </html>
+     </ConvexAuthNextjsServerProvider>
+   );
+ }
