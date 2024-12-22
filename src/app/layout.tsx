abefd8d4abbb3fdf6ccde1414815f0980802bc96
Humesh Deshmukh <112746090+humeshdeshmukh@users.noreply.github.com>
2024-12-22 21:20:51 +0530

    new changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
 import { cn } from "@/lib/utils";
 import { Noto_Sans } from 'next/font/google';
+// import { Analytics } from "@/lib/analytics/Analytics";
 import { generateMetadata } from "@/lib/generateMetadata";
 import { ConvexClientProvider } from "@/providers/ConvexAuthProvider";
 import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
@@ -12,6 +13,7 @@ import NextTopLoader from 'nextjs-toploader';
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { Toaster } from "@/components/ui/sonner";
+
 
 const font = Noto_Sans({ weight: ['400', '700'], subsets: ['latin'] });
 
@@ -26,13 +28,14 @@ export default function RootLayout({
     <ConvexAuthNextjsServerProvider>
       <html lang="en" suppressHydrationWarning>
         <body
-          className={cn(font.className, "antialiased", "bg-gray", "dark:bg-gray-900")}
+          className={cn(font.className, "antialiased")}
         >
           <ConvexClientProvider>
+            {/* TODO: Use a different color here based on your app theme */}
             <NextTopLoader showSpinner={false} color="#fcba28" />
             <Toaster />
             <Header />
-            <main className="container mx-auto p-4">{children}</main>
+            {children}
             <Footer />
           </ConvexClientProvider>
         </body>
