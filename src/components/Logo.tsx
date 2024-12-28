b084e8c6bb7d155e2ad0195ab7c80b10ff823f84
Humesh Deshmukh <112746090+humeshdeshmukh@users.noreply.github.com>
2024-12-28 17:47:03 +0530

    5.2.1.1

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 
+// TODO: Replace this with your own logo
 // TODO: I like to create a component for this, so I can reuse it in other places
 
 // To get your logos head over: SVG Repo, UntitledUI/logos these are great website to get your logos
@@ -12,9 +13,8 @@ export const Logo = ({ className }: { className?: string; }) => {
             <Image
                 width={50}
                 height={50}
-                src="/logo1.png"
-                alt="InterviewMaster.ai"
-                priority
+                src="/logo.svg"
+                alt="Dev Logo"
             />
             <p className="font-semibold leading-relaxed text-2xl">interviewmaster.ai</p>
         </Link>
